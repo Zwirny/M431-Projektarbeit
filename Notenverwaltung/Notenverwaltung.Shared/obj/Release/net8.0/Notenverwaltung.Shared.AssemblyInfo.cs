e078c23027f845c187173377e096c44333d7c5f7
Sandro Zwyer <227748999+Zwirny@users.noreply.github.com>
2026-01-10 23:29:28 +0100

    Cleanup Files & Resolved Error

--- Notenverwaltung/Notenverwaltung.Shared/obj/Release/net8.0/Notenverwaltung.Shared.AssemblyInfo.cs
+++ Notenverwaltung/Notenverwaltung.Shared/obj/Release/net8.0/Notenverwaltung.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notenverwaltung.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21e936be1ec15e23913cd7b0d5c4617af0a4e19b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720f9a6d2581cf2b70f17673ec49e6e1516699b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notenverwaltung.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notenverwaltung.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Notenverwaltung/Notenverwaltung.Web/obj/Debug/net8.0/Notenverwaltung.Web.AssemblyInfo.cs
+++ Notenverwaltung/Notenverwaltung.Web/obj/Debug/net8.0/Notenverwaltung.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notenverwaltung.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cabcb09ec36dbf4fb00160abfe2097392722cabc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720f9a6d2581cf2b70f17673ec49e6e1516699b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notenverwaltung.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notenverwaltung.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
