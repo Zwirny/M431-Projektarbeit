0d0f6eb13d5cf38c51bf27d08d2ff0ac22015906
Sandro Zwyer <227748999+Zwirny@users.noreply.github.com>
2026-01-11 00:39:33 +0100

    Add rounding grades

--- Notenverwaltung/Notenverwaltung.Web/obj/Debug/net8.0/Notenverwaltung.Web.AssemblyInfo.cs
+++ Notenverwaltung/Notenverwaltung.Web/obj/Debug/net8.0/Notenverwaltung.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notenverwaltung.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720f9a6d2581cf2b70f17673ec49e6e1516699b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e078c23027f845c187173377e096c44333d7c5f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notenverwaltung.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notenverwaltung.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
