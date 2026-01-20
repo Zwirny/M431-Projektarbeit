a8a8d0ceb400d9d61e68fe61b272436510dd3438
Sandro Zwyer <227748999+Zwirny@users.noreply.github.com>
2026-01-20 23:44:21 +0100

    Add GetCourse-Service

---
+++ Notenverwaltung/Notenverwaltung.Web/Services/CourseService.cs
@@ -0,0 +1,19 @@
+﻿using Notenverwaltung.Shared.Dtos.CourseDtos;
+using System.Net.Http.Json;
+
+namespace Notenverwaltung.Web.Services;
+
+public class CourseService
+{
+    private readonly HttpClient _httpClient;
+    public CourseService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<HttpResponseMessage> GetCoursesAsync()
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync("api/Course/getCourses");
+        return response;
+    }
+}
