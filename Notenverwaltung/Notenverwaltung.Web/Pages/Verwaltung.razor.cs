2b5b7a1efc658163b38a7dfc79fab3f58eba9dd8
Sandro Zwyer <227748999+Zwirny@users.noreply.github.com>
2026-01-20 21:48:07 +0100

    Fix rounding & attempt fix log out button

--- Notenverwaltung/Notenverwaltung.Web/Pages/Verwaltung.razor.cs
+++ Notenverwaltung/Notenverwaltung.Web/Pages/Verwaltung.razor.cs
@@ -10,5 +10,13 @@ public partial class Verwaltung
     public void RoundReportGrade()
     {
         reportGrade = Math.Round(reportGrade * 2, MidpointRounding.AwayFromZero) / 2;
+        if(reportGrade > 6)
+        {
+            reportGrade = 6;
+        }
+        else if(reportGrade < 1)
+        {
+            reportGrade = 1;
+        }
     }
 }
